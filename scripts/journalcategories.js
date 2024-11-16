64e62d7cf0f91984a1e2b5bd16afb6c093bce910
Ingo Taag <ingo.taag@synpulse8.de>
2024-11-16 19:14:27 +0100

    transferred code to class file

--- scripts/journalcategories.js
+++ scripts/journalcategories.js
@@ -1,3 +1,68 @@
 export class JournalCategories {
-   
+   static renderPage(app, html, data)  {
+        // Überprüfen, ob der Editor-Modus aktiv ist
+        const journalPage = app.object;
+        if (!app.options.editable) {
+            const category = journalPage.getFlag('journal-categories', 'categoryDropdown') || "-";
+            const categoryIndex = valueSet.indexOf(category);
+            const iconClass = iconSet[categoryIndex] || "";
+
+        }else {
+            const werteText = game.settings.get("journal-categories", "valueSet");
+            const werteArray = werteText.split(";").map(item => item.trim()); // Array der Werte aus werteText
+            // Überprüfen, ob das Dropdown-Feld bereits existiert, um Duplikate zu vermeiden
+            if (html.find(".category-dropdown").length === 0) {
+                // Dropdown-Feld erstellen
+                let dropdownHTML = `
+                    <div class="journal-categories-dropbdown-group form-group">
+                        <label for="categoryDropdown">Kategorie</label>
+                        <select name="flags.categoryDropdown" id="categoryDropdown" class="category-dropdown">
+                            <option value="">-- Bitte wählen --</option>`;        
+                            werteArray.forEach(value => {
+                                dropdownHTML += `<option value="${value}" ${journalPage.getFlag('journal-categories', 'categoryDropdown') === value ? "selected" : ""}>${value}</option>`;
+                            });
+                            dropdownHTML += `
+                        </select>
+                    </div>
+                `;
+
+                // Dropdown in den Journal-Editor einfügen
+                html.find("input[name='name']").parent().after(dropdownHTML);
+      
+                // Daten aktualisieren bei Änderung
+                html.find(".category-dropdown").on("change", async (event) => {
+                    const selectedValue = event.target.value;
+                    await journalPage.setFlag('journal-categories', 'categoryDropdown', selectedValue);
+                });
+            }
+        };
+    }
+    
+    static renderSheet (app, html, data) {
+        const journalEntries = app.object.pages.contents;
+        const valueSet = game.settings.get("journal-categories", "valueSet").split(";").map(item => item.trim());
+        const iconSet = game.settings.get("journal-categories", "iconSet").split(";").map(icon => icon.trim());
+
+      
+        // Schleife durch alle Seiten des Journals
+        journalEntries.forEach((page,index) => {
+          // Den gespeicherten Wert der Kategorie für jede Seite abrufen
+          const category = page.getFlag('journal-categories', 'categoryDropdown') || "-";
+          const categoryIndex = valueSet.indexOf(category);
+          const iconClass = iconSet[categoryIndex] || "";
+
+          const pageTitle = html.find(`.directory-item[data-page-id="${page.id}"] .page-title`);
+      
+          // Finde das Element der Seitenliste und füge die Kategorie daneben hinzu
+          if (pageTitle.length > 0) {
+            // Icon-Anzeige in .page-title erstellen und hinzufügen, falls sie noch nicht existiert
+            if (!pageTitle.find('.category-display-icon').length) {
+              pageTitle.prepend(`
+                <i class="${iconClass} category-display-icon" title="${category}" style="margin-right: 5px;"></i>
+              `);
+            }
+          }
+        });
+    }
 }
+
